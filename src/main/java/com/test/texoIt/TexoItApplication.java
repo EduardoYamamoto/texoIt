e17188a69d29916f6a6952ac3c022e3a0a1ce465
Eduardo Yamamoto <edukendi@gmail.com>
2022-11-02 20:29:03 -0300

    Initial commit

---
+++ src/main/java/com/test/texoIt/TexoItApplication.java
@@ -0,0 +1,13 @@
+package com.test.texoIt;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TexoItApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TexoItApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/test/texoIt/TexoItApplicationTests.java
@@ -0,0 +1,13 @@
+package com.test.texoIt;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TexoItApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
